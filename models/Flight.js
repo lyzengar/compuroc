066e94383940d9839a25f749c4d51ff5c7a8bbc0
lyzengar <darthmaul85@hotmail.com>
2019-02-13 13:45:34 -0800

    update validation

--- models/Flight.js
+++ models/Flight.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const FlightSchema = new mongoose.Schema({
     diameter: {type: Number, required: true},
     mass: {type: Number, required: true},
-    dragCoef: {type: Number, required: true},
+    dragCoef: {type: Number, required: true, enum:[0.25, 0.30, 0.35, 0.40, 0.45, 0.50, 0.55, 0.60, 0.65, 0.70, 0.75, 0.80, 0.85]},
     motorManu: {type: String, required: true},
     motorLetter: {type: String, required: true},
     motorClass: {type: String, required: true},
